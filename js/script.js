9e04dd58464499e46f73ff6f7965e1ec5436a365
overoid <110168178+overoid@users.noreply.github.com>
2022-08-01 18:21:41 +0300

    hw3 done

---
+++ js/script.js
@@ -0,0 +1,142 @@
+//1
+
+
+// let str = 'The Stains of Time';
+// console.log(str)
+// let number = 7;
+// console.log(number)
+// let bigInt = BigInt(42);
+// console.log(bigInt)
+// let symbol = Symbol('Red Sun');
+// console.log(symbol)
+// let under;
+// console.log(under)
+// let isBool = true;
+// console.log(isBool)
+// let nul = null;
+// console.log(nul)
+// let obj = {
+//     Jet: 'Sam'}
+// console.log(obj)
+
+//2
+//
+// var varVariable = 24;
+// varVariable = 'here';
+// console.log(varVariable)
+//
+// let letVariable = 'here';
+// letVariable = 7;
+// console.log(letVariable)
+
+
+
+
+//3
+// -
+//4
+
+// let strPlusNum = 24142 + 53;
+// console.log(strPlusNum)
+// let strPlusBigint = 2 + 2424;
+// console.log(strPlusBigint)
+// let strPlusSym = 13 + 25;
+// console.log(strPlusSym)
+// let strPlusUnder = 53 + 24;
+// console.log(strPlusUnder)
+// let strPlusBool = 242 + 141;
+// console.log(strPlusBool)
+// let strPlusNull = 6 + 24;
+// console.log(strPlusNull)
+// let strPlusObj = 14 + 636;
+// console.log(strPlusObj)
+// let NumPlusBigint = 636+252;
+// console.log(NumPlusBigint)
+// let NumPlusSym = 525 + 25;
+// console.log(NumPlusSym)
+// let NumPlusBool = 25 + 36;
+// console.log(NumPlusBool)
+// let NumPlusNull = 3 + 99;
+// console.log(NumPlusNull)
+// let NumPlusObj = 7 + 228;
+// console.log(NumPlusObj)
+// let NumPlusUnder = 1 + 63;
+// console.log(NumPlusUnder)
+
+//5
+
+// let strMinusNum = 22 - 11;
+// console.log(strMinusNum)
+// let strMinusBigint = 111 - 52;
+// console.log(strMinusBigint)
+// let strMinusSym = 52 - 111;
+// console.log(strMinusSym)
+// let strMinusUnder = 25 - 35;
+// console.log(strMinusUnder)
+// let strMinusBool = 84 - 363;
+// console.log(strMinusBool)
+// let strMinusNull = 353 - 241;
+// console.log(strMinusNull)
+// let strMinusObj = 53 - 1535;
+// console.log(strMinusObj)
+// let NumMinusStr = 98 - 335;
+// console.log(NumMinusStr)
+// let NumMinusBigint = 63 - 228;
+// console.log(NumMinusBigint)
+// let NumMinusSym = 1 - 12;
+// console.log(NumMinusSym)
+// let NumMinusUnder = 754 - 6;
+// console.log(NumMinusUnder)
+// let NumMinusBool = 36 - 87;
+// console.log(NumMinusBool)
+// let NumMinusNull = 14 - 9;
+// console.log(NumMinusNull)
+// let NumMinusObj = 5 - 332;
+// console.log(NumMinusObj)
+
+//6
+// const login = prompt('Enter login');
+// const password =  prompt('Enter password');
+// if (login === 'kykold' && password === '1234asdQQ'){
+//     alert('Добро пожаловать')
+// }else {
+//     alert('Ошибка входа')
+// }
+
+//7
+// let minAge = 16;
+// let age = +prompt('Enter your age');
+// if (age >= minAge){
+//     alert('Ошибка входа')
+// }else (alert('вход воспрещен'))
+
+//8
+// let experience = +prompt('Enter your experience by number')
+// if (experience < 5){
+//     alert('У Вас нет надббавки, вы только начинаете свой путь!')
+// }else if (experience <= 7){
+//     alert('Ваша надбавка к з/п составляет 6%')
+// }else if (experience <= 13 ){
+//     alert('Ваша надбавка к з/п составляет 10%')
+// }else {alert('Ваша надбавка к з/п составляет 23%')}
+
+//9
+// let numberOfWeekDay = +prompt('Enter number of weekDay');
+// switch (numberOfWeekDay) {
+//     case 1 : alert('Monday');
+//         break;
+//     case 2 : alert('Tuesday');
+//         break;
+//     case 3 : alert('Wednesday');
+//         break;
+//     case 4 : alert('Thursday');
+//         break;
+//     case 5 : alert('Friday');
+//         break;
+//     case 6 : alert('Saturday');
+//         break;
+//     case 7 : alert('Sunday');
+//         break;
+//     default : alert('Your number is not correct');
+//         break;
+// }
